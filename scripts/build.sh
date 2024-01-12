fdb8b1deed662d614de45b4459183aca8929ae43
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-01-12 14:03:27 +0000

    fix: update build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,53 +4,55 @@ program_name="$(basename $(pwd))"
 version=0.0.1
 
 build_libs() {
-    gcc -c -std=c99 -o build/debug/lib/termbox2.o libs/termbox2.c
-    g++ -c -std=c++17 -o build/debug/lib/argparse.o libs/argparse.cpp
-    g++ -c -std=c++17 -o build/debug/lib/httplib.o libs/httplib.cpp
+    lib_dir="build/$1/lib"
+
+    if ! [ -f "$lib_dir/termbox2.o" ]; then
+        gcc -c -std=c99 -o $lib_dir/termbox2.o libs/termbox2.c
+    fi
+    if ! [ -f "$lib_dir/argparse.o" ]; then
+        g++ -c -std=c++17 -o $lib_dir/argparse.o libs/argparse.cpp
+    fi
+    if ! [ -f "$lib_dir/httplib.o" ]; then
+        g++ -c -std=c++17 -o $lib_dir/httplib.o libs/httplib.cpp
+    fi
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to compile libs"; exit 1
+    fi
 }
 
 mkdir -p build
 
 if [ "$1" = "-r" ]; then
     # Build for release
-    rm -rf build/release 2> /dev/null || true && mkdir -p build/release
-    mkdir build/release/{bin,lib}
+    rm -rf build/release/bin 2> /dev/null || true && mkdir -p build/release
+    mkdir -p build/release/{bin,lib}
 
-    build_libs
-    g++ -c -std=c++17 -o build/debug/lib/main.o main.cpp
+    build_libs "release"
+    g++ -c -std=c++17 -o build/release/lib/main.o main.cpp
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to compile main"; exit 1
+    fi
 
     if [ "$(uname)" = "Darwin" ]; then
         g++ -O3 -std=c++17 \
             -o build/release/bin/$program_name \
-            build/debug/lib/*.o \
+            build/release/lib/*.o \
             -lssl -lcrypto -framework CoreFoundation -framework Security
     else
-        if [ "$2" = "-s" ]; then
-            # Copy static libs
-            cp -L /usr/lib/x86_64-linux-gnu/{libssl,libcrypto}.a build/release/lib
+        # Copy static libs
+        cp -L /usr/lib/x86_64-linux-gnu/{libssl,libcrypto}.a build/release/lib
 
-            if [ $? -ne 0 ]; then
-                echo "Failed to copy static libs"; exit 1
-            fi
-
-            g++ -O3 -std=c++17 -static-libgcc -static-libstdc++ \
-                -pthread \
-                build/debug/lib/*.o \
-                -L build/release/lib -l:libssl.a -l:libcrypto.a -ldl \
-                -o build/release/bin/$program_name
-        else
-            # Copy dynamic libs
-            cp -L /usr/lib/x86_64-linux-gnu/{libssl,libcrypto}.so.* build/release/lib
-
-            if [ $? -ne 0 ]; then
-                echo "Failed to copy dynamic libs"; exit 1
-            fi
-
-            g++ -O3 -std=c++17 -static-libgcc -static-libstdc++ \
-                -pthread -Wl,-rpath,\$ORIGIN/../lib/ \
-                -o build/release/bin/$program_name *.cpp \
-                -L./build/release/lib -lssl -lcrypto
+        if [ $? -ne 0 ]; then
+            echo "Failed to copy static libs"; exit 1
         fi
+
+        g++ -O3 -std=c++17 -static-libgcc -static-libstdc++ \
+            -pthread \
+            build/release/lib/*.o \
+            -L build/release/lib -l:libssl.a -l:libcrypto.a -ldl \
+            -o build/release/bin/$program_name
     fi
 
     if [ $? -ne 0 ]; then
@@ -62,11 +64,15 @@ if [ "$1" = "-r" ]; then
     fi
 elif [ "$1" = "-d" ]; then
     # Build for debug
-    rm -rf build/debug 2> /dev/null || true && mkdir -p build/debug
+    rm -rf build/debug/bin 2> /dev/null || true && mkdir -p build/debug
     mkdir -p build/debug/{bin,lib}
 
-    build_libs
+    build_libs "debug"
     g++ -c -std=c++17 -o build/debug/lib/main.o main.cpp
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to compile main"; exit 1
+    fi
 
     if [ "$(uname)" = "Darwin" ]; then
         g++ -g -std=c++17 \
@@ -81,6 +87,6 @@ elif [ "$1" = "-d" ]; then
             -lssl -lcrypto
     fi
 else
-    echo "Usage: $0 [-r] [-d] [-s]"
+    echo "Usage: $0 [-r] [-d]"
     exit 1
 fi
